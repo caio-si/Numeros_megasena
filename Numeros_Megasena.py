9b7ca25d2f84fbcb282518d3bb27d49fb50f117f
Caio Silva <75350795+caio-si@users.noreply.github.com>
2022-05-10 23:18:48 -0300

    Add files via upload

---
+++ Numeros_Megasena.py
@@ -0,0 +1,13 @@
+
+import random
+
+def megasena():
+   jogo = []
+   while len(jogo) < 6:
+       num = random.randint(1,60)
+       if num in jogo:
+           continue
+       else:
+           jogo.append(num)
+           print(sorted(jogo))
+print(megasena())
